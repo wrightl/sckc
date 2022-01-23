113cdeb1819fb450b437e3ec0ae85c6d94f7eddc
wrightl <leewright76@hotmail.com>
2022-01-23 18:52:54 +0000

    build changes

--- sckc-app/src/app/app.module.ts
+++ sckc-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +28,7 @@ import { ActivitiesComponent } from './activities/activities.component';
     MatToolbarModule,
     MatTreeModule,
   ],
-  providers: [],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/app' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
