b0ef9c7fbf58d96d7ffe2a844c628ce9be90edfd
wrightl <leewright76@hotmail.com>
2022-01-23 18:08:15 +0000

    remove lifecycle method

--- sckc-app/src/app/activities/activities.component.ts
+++ sckc-app/src/app/activities/activities.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-activities',
   templateUrl: './activities.component.html',
-  styleUrls: ['./activities.component.scss']
+  styleUrls: ['./activities.component.scss'],
 })
-export class ActivitiesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ActivitiesComponent {
+  constructor() {}
 }
