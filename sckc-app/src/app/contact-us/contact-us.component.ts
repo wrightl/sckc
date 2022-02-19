103e190f19a170bb01b6244b70e2048388f7f92d
wrightl <leewright76@hotmail.com>
2022-02-19 16:57:36 +0000

    contact us updated

--- sckc-app/src/app/contact-us/contact-us.component.ts
+++ sckc-app/src/app/contact-us/contact-us.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { MatFormFieldControl } from '@angular/material/form-field';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -9,14 +8,30 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./contact-us.component.scss'],
 })
 export class ContactUsComponent {
-  public name: string = '';
+  name = '';
   email = '';
   subject = '';
   message = '';
+  error = '';
+  response = '';
 
   constructor(private http: HttpClient) {}
 
   sendMessage() {
+    this.error = '';
+    this.response = '';
+
+    let errorMessage = '';
+    if (!this.name) errorMessage += 'Please enter your name<br/>';
+    if (!this.email) errorMessage += 'Please enter an email address<br/>';
+    if (!this.subject) errorMessage += 'Please enter a subject<br/>';
+    if (!this.message) errorMessage += 'Please enter a message<br/>';
+
+    if (errorMessage) {
+      this.error = errorMessage;
+      return false;
+    }
+
     this.http
       .post(`${environment.baseApiUrl}ContactUs`, {
         name: this.name,
@@ -24,9 +39,19 @@ export class ContactUsComponent {
         subject: this.subject,
         message: this.message,
       })
-      .subscribe((result) => {
-        console.log(result);
-      });
+      .subscribe(
+        (result) => {
+          this.response =
+            'Thanks for getting in touch. Someone will reply to your email soon';
+          this.name = '';
+          this.subject = '';
+          this.email = '';
+          this.message = '';
+        },
+        (error) =>
+          (this.error =
+            'Unfortunately an error occurred. Please try again, or send an email to <a href="mailto:enquiries@sheffieldcitykayakclub.co.uk">enquiries@sheffieldcitykayakclub.co.uk</a>')
+      );
 
     return false;
   }
