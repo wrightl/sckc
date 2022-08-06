22090733653205be9277850400acb1b7b03fbdc5
Lee Wright <lee.wright@tribalgroup.com>
2022-08-06 14:56:17 +0100

    added homepage images

--- sckc-app/src/app/home/home.component.ts
+++ sckc-app/src/app/home/home.component.ts
@@ -4,12 +4,32 @@ import { HOME_IMAGES } from '../data/home-images';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  images: string[];
 
-  images = HOME_IMAGES;
+  constructor() {
+    this.images = this.shuffle(HOME_IMAGES);
+  }
 
-  constructor() { }
+  shuffle(array: string[]) {
+    let currentIndex = array.length,
+      randomIndex;
 
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
 }
