4d1d302fe944ea7a5b373087d50cce5cb29ffe8a
Lee <leewright76@hotmail.com>
2024-04-02 18:21:11 +0100

    update pool pricing

--- sckc-app/src/app/data/pool.ts
+++ sckc-app/src/app/data/pool.ts
@@ -1,12 +1,10 @@
 export const POOLINFO: PoolInfo = {
-  adultHalfSession: 10,
+  adultThirtyMinuteSession: 6,
+  adultSixtMinuteSession: 12,
   adultFullSession: 16,
-  childHalfSession: 4,
+  childThirtyMinuteSession: 3,
+  childSixtyMinuteSession: 6,
   childFullSession: 7,
-  adultHalfSessionNonMember: 12,
-  adultFullSessionNonMember: 18,
-  childHalfSessionNonMember: 6,
-  childFullSessionNonMember: 9,
 };
 
 export const POOL_BOOKING_ITEMS: BookingItem[] = [
@@ -20,27 +18,15 @@ export const POOL_BOOKING_ITEMS: BookingItem[] = [
     cost: POOLINFO.childFullSession,
     quantity: 0,
   },
-  {
-    description: 'Adult Non-Member',
-    cost: POOLINFO.adultFullSessionNonMember,
-    quantity: 0,
-  },
-  {
-    description: 'Child Non-Member',
-    cost: POOLINFO.childFullSessionNonMember,
-    quantity: 0,
-  },
 ];
 
 export interface PoolInfo {
-  adultHalfSession: number;
+  adultThirtyMinuteSession: number;
+  adultSixtMinuteSession: number;
   adultFullSession: number;
-  childHalfSession: number;
+  childThirtyMinuteSession: number;
+  childSixtyMinuteSession: number;
   childFullSession: number;
-  adultHalfSessionNonMember: number;
-  adultFullSessionNonMember: number;
-  childHalfSessionNonMember: number;
-  childFullSessionNonMember: number;
 }
 
 export interface BookingItem {
